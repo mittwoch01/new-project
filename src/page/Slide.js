3301c45786f82635126268b1b26bbbee73ffe8c5
mittwoch01 <wenigwaves@gmail.com>
2022-01-20 22:12:20 +0900

    [add]Slide:ab:slide를 만들 ul과 li
    slide를 만들 next, prev버튼과 slide1,2,3,4 설정

--- src/page/Slide.js
+++ src/page/Slide.js
@@ -5,11 +5,16 @@ export default function Slide() {
   return (
     <div id="mainSlideBox">
       <div className="slide_btn">
-        <button type="button" className="left"></button>
-        <button type="button" className="right"></button>
+        <button type="button" className="next">다음</button>
+        <button type="button" className="prev">이전</button>
       </div>
       <div className="slide_content">
-
+        <ul>
+          <li>01</li>
+          <li>02</li>
+          <li>03</li>
+          <li>04</li>
+        </ul>
       </div>
       <ul className="slider-dot">
         <li></li>
